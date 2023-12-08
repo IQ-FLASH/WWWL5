ccc56da5e8860bd7987cea8a2580b04ad90b492d
IQ-FLASH <153175039+IQ-FLASH@users.noreply.github.com>
2023-12-08 19:54:52 +0200

    Add files via upload

--- WWWL5/plugins/مرح.py
+++ WWWL5/plugins/مرح.py
@@ -192,7 +192,7 @@ async def permalink(mention):
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     sos = random.choice(rr7)
     await edit_or_reply(
-        mention, f"- نسبة الجمال لـ [{muh}](tg://user?id={user.id}) هـي {sos} 🫣🖤"
+        mention, f"نـسـبـة الـجـمـال [{muh}](tg://user?id={user.id}) هـي {sos} 🫣🖤"
     )
 
 
@@ -210,7 +210,7 @@ async def permalink(mention):
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     sos = random.choice(rr7)
     await edit_or_reply(
-        mention, f"- نسبة الغباء لـ [{muh}](tg://user?id={user.id}) هـي {sos} 😂🖤"
+        mention, f"نـسـبـة الـغـبـاء [{muh}](tg://user?id={user.id}) هـي {sos} 😂🖤"
     )
 
 
